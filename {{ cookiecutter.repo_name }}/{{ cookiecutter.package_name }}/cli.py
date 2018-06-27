cba93020d93f112d6a5e98429dacf4707b21c56f
Joe Moore <jpvlsmv@gmail.com>
2018-06-27 16:40:25 -0400

    Add logging (#6)
    * Add logging by default

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/cli.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/cli.py
@@ -1,10 +1,16 @@
 import click
-
+import logging
+logger = logging.getLogger()
 
 @click.command()
 @click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
+@click.option('--loglevel', '-ll', nargs=2, type=click.Choice(['DEBUG','INFO','WARNING','ERROR','CRITICAL']))
 @click.argument('name', default='world', required=False)
-def main(name, as_cowboy):
+def main(name, loglevel, as_cowboy):
     """{{ cookiecutter.project_short_description }}"""
+    logging.basicConfig(level=loglevel)
+
     greet = 'Howdy' if as_cowboy else 'Hello'
+
+    logger.debug('Sending greeting')
     click.echo('{0}, {1}.'.format(greet, name))
